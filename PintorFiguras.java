1b85679237b4699227c149a1ae139b9aeef9be78
David <dreguilonTdaw1@gmail.com>
2020-11-13 22:01:14 +0100

    trabajo bucles

--- PintorFiguras.java
+++ PintorFiguras.java
@@ -11,7 +11,9 @@ public class PintorFiguras
     private static final char ESPACIO = ' ';
     private static final char CAR1 = 'B';
     private static final char CAR2 = '=';
-
+    //Atributos que deberian ser variables locales
+    int coef;
+    int altura;
 
     /**
      * Dibuja una figura (ver enunciado) de altura
@@ -20,14 +22,47 @@ public class PintorFiguras
      * (usa bucles for)
      */
     public  void dibujarFigura(int altura) {
-        
-    }
+        int coef;//Para cuando usemos una pirámide de 8 de altura por ejemplo(pares)
+            if (altura % 2 == 0)
+                coef = 1;
+            else
+                coef = 0;
+            
+            for(int i=0;i<=(int)(altura)-coef;i++){
+                for(int j=0;j<(int)(altura
+                
+                
+                )-i;j++){
+                    System.out.print(" ");
+                }
+                for(int j=0;j<=2*i;j++){
+                    System.out.print("*");
+                }
+                for(int j=(int)(altura)+i+1;j<altura;j++){
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+
+      }
+    
 
     /**
      * Método privado de ayuda que escribe n espacios en la misma línea
      */
     private  void escribirEspacios(char caracter, int n) {
-         
+         for(int i=0;i<=(int)(altura) - coef;i++){
+                for(int j=0;j<(int)(altura
+                
+                
+                )-i;j++){
+                    System.out.print(" ");
+                }
+                for(int j=(int)(altura)+i+1;j<altura;j++){
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
 
-    }
+     }
 }
--- Utilidades.java
+++ Utilidades.java
@@ -1,25 +1,45 @@
-import java.util.Scanner;
-/**
- *   Clase de utilidades
- *   Incluye métodos estáticos
- *   
- *   @author - 
- */
-public class Utilidades
-{
-
+    import java.util.Scanner;
     /**
-     * Dado un número n (asumimos positivo)
-     * devueve true si está en octal false en otro caso
-     * Un nº está en octal si cada una de sus cifras
-     * es un valor entre 0 y 7
-     * 
-     * (usa bucles while)
+     *   Clase de utilidades
+     *   Incluye métodos estáticos
+     *   
+     *   @author - 
      */
-    public static boolean estaEnOctal(int n) {
-         
-        return true;
-
+    public class Utilidades
+{
+    
+        /**
+         * Dado un número n (asumimos positivo)
+         * devueve true si está en octal false en otro caso
+         * Un nº está en octal si cada una de sus cifras
+         * es un valor entre 0 y 7
+         * 
+         * (usa bucles while)
+         */
+        public static boolean estaEnOctal(int n) {
+            
+        boolean octal = false;
+        
+        do{int cifra = n / 10;
+         if (cifra >= 0 && cifra <= 7){
+             octal = true;
+            }
+         else{
+             octal = false;
+             return false;
+             }
+            if(n /10 < 8)
+            {octal = true;}
+             else{octal = false;break;
+         }
+        }
+         while(n!=0 && n >=8);{
+             
+         return true;
+        }
+        
+       
+    
     }
 
     /**
@@ -28,9 +48,15 @@ public class Utilidades
      * 
      * (usa bucles while)
      */
+    
     public static int contarCifras(int n) {
-         
-        return 0;
+            int contador = 0;
+         while(n>10){
+             n = n/10;
+             contador++;
+             if(n % 2 > 0 && n % 2 < 10 ){contador++;}
+            }
+        return contador;
 
     }
 
